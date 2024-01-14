541e53eddef15ea93bb5c3670d09ad86a63ea9d6
AnCapone <okai.kot44@gmail.com>
2024-01-14 14:21:07 +0200

    add comments to Third Task

--- thirdTask.py
+++ thirdTask.py
@@ -7,23 +7,27 @@ readedString = ""
 excludedWord = ""
 numEntries = 0
 
-with open("thirdText.txt", "r") as myFile:
+with open("thirdText.txt", "r") as myFile:  # читаємо текст з файлу та зберігаємо його в рядок
     readedString = myFile.read()
-
-while True:
+# далі будемо працювати з вмістом файла, як з рядком
+while True:  # цикл для запиту від користувача та первинної обробки слова, яке ввів користувач
     excludedWord = input("Введіть заборонене слово: ")
-    if len(excludedWord) == 0:
+    if len(excludedWord) == 0:  # якщо користувач не ввів нічого
         print("Веедіть слово, що складається щонайменше з 1 букви!")
         continue
-    elif re.fullmatch(r'\b\w+\b', excludedWord) is None:
+    elif re.fullmatch(r'\b\w+\b', excludedWord) is None:  # перевірка, за допомогою регулярного виразу
+                                                                 # чи є словом, а не набором спецсимволів
+                                                                 # введений рядок
         print("Введений рядок не є словом")
         continue
+    break
 
-numEntries = readedString.count(excludedWord)
+numEntries = readedString.count(excludedWord)  # порахуємо кількість входжень забороненого слова
 
-if numEntries == 0:
+if numEntries == 0:  # якщо заборонене слово не знайдено в тексті, повідомимо про це та завершимо виконання програми
     print("Введене заборонене слово не знайдено! Вхідний текст не змінено!")
-else:
+elif numEntries > 0:  # якщо заборонене слово знайдено, виконаємо заміну цього слова на "зірочки"
     readyString = readedString.replace(excludedWord, "*" * len(excludedWord))
     with open("thirdText.txt", "w") as myFile:
         myFile.write(readyString)
+    print(f"Статистика: Виконано {numEntries} замін(и) забороненого слова {excludedWord}")
